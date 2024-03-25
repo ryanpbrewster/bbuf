3f30138abb477e6515475a5234bf0833b3661bd4
Ryan P. Brewster <RyanPBrewster@gmail.com>
2024-03-24 22:37:22 -0400

    Allow specifying delay via CLI flag

--- cmd/example1/example1.go
+++ cmd/example1/example1.go
@@ -4,24 +4,28 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"time"
 
 	"github.com/ryanpbrewster/bbuf/sink"
 )
 
 func main() {
+	delay := flag.Duration("delay", 100*time.Millisecond, "how slow to make the flush")
 	buffered := flag.Bool("buffered", false, "should we use a bbuf.Sink or not?")
 	flag.Parse()
 
-	var out io.Writer = &slowDevNull{}
+	var out io.Writer = &slowDevNull{delay: *delay}
 	if *buffered {
-		fmt.Println("creating a buffered out")
-		s := sink.New(out, sink.WithOverflow(sink.Discard))
+		log.Println("creating a buffered out")
+		s := sink.New(out, sink.WithCapacity(8096), sink.WithOverflow(sink.Flush))
 		defer s.Close()
 		out = s
+	} else {
+		log.Println("leaving output unbuffered")
 	}
 
-	payload := make([]byte, 100)
+	payload := make([]byte, 1)
 
 	t := time.NewTicker(1 * time.Millisecond)
 	defer t.Stop()
@@ -30,10 +34,12 @@ func main() {
 	}
 }
 
-type slowDevNull struct{}
+type slowDevNull struct {
+	delay time.Duration
+}
 
 func (s *slowDevNull) Write(p []byte) (n int, err error) {
-	time.Sleep(1_000 * time.Millisecond)
+	time.Sleep(s.delay)
 	fmt.Printf("%d bytes\n", len(p))
 	return len(p), nil
 }
